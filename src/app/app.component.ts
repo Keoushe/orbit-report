3dd6a5e1e97cdee81f83104cc258ae4fa340b709
Michael Patterson <keoushe@gmail.com>
2021-04-07 13:49:39 -0500

    finished search bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayList: Satellite[] = [];
   title = 'orbit-report';
   sourceList: Satellite[];
   constructor() {
@@ -18,11 +19,34 @@ export class AppComponent {
  
           let fetchedSatellites = data.satellites;
           // TODO: loop over satellites
-          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
-          // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
- 
+          for (let i = 0; i < fetchedSatellites.length; i++){
+            // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+            let satellite = new Satellite(
+              fetchedSatellites[i].name, 
+              fetchedSatellites[i].type, 
+              fetchedSatellites[i].launchDate, 
+              fetchedSatellites[i].orbitType, 
+              fetchedSatellites[i].operational)
+            // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+            this.sourceList.push(satellite)
+          }
+          this.displayList = this.sourceList.slice(0);
+
        }.bind(this));
     }.bind(this));
  
  }
+ search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
+}
 }
--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -12,4 +12,6 @@ constructor(name: string, type: string, launchDate: string, orbitType: string, o
     this.operational = operational;
 }
 
+shouldShowWarning(): boolean{
+    return this.type.toLowerCase() === 'space debris';}
 }
